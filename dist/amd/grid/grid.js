af0cafc743dc1f290673b31d9d0d46363bbb71e7
charlespockert <charles.pockert@googlemail.com>
2015-07-20 09:55:44 +0100

    Sync column header widths with cells
    Fixes #34

--- dist/amd/grid/grid.js
+++ dist/amd/grid/grid.js
@@ -78,6 +78,7 @@ define(['exports', 'aurelia-framework', './grid-column', 'gooy/aurelia-compiler'
 			this.data = [];
 			this.count = 0;
 			this.unbinding = false;
+			this.scrollBarWidth = 16;
 
 			this.element = element;
 			this.compiler = compiler;
@@ -208,6 +209,8 @@ define(['exports', 'aurelia-framework', './grid-column', 'gooy/aurelia-compiler'
 				this.updatePager();
 
 				this.watchForChanges();
+
+				setTimeout(this.syncColumnHeadersWithColumns.bind(this), 0);
 			}
 		}, {
 			key: 'applyPage',
@@ -409,7 +412,6 @@ define(['exports', 'aurelia-framework', './grid-column', 'gooy/aurelia-compiler'
 		}, {
 			key: 'gridHeightChanged',
 			value: function gridHeightChanged() {
-
 				var cont = this.element.querySelector('.grid-content-container');
 
 				if (this.gridHeight > 0) {
@@ -417,6 +419,32 @@ define(['exports', 'aurelia-framework', './grid-column', 'gooy/aurelia-compiler'
 				} else {
 					cont.removeAttribute('style');
 				}
+			}
+		}, {
+			key: 'syncColumnHeadersWithColumns',
+			value: function syncColumnHeadersWithColumns() {
+				var headers = this.element.querySelectorAll('table>thead>tr:first-child>th');
+				var filters = this.element.querySelectorAll('table>thead>tr:last-child>th');
+
+				var cells = this.element.querySelectorAll('table>tbody>tr:first-child>td');
+
+				for (var i = headers.length - 1; i >= 0; i--) {
+					var header = headers[i];
+					var filter = filters[i];
+					var cell = cells[i];
+					var overflow = this.isBodyOverflowing();
+
+					var tgtWidth = cell.offsetWidth + (i == headers.length - 1 && overflow ? this.scrollBarWidth : 0);
+
+					header.setAttribute('style', 'width: ' + tgtWidth + 'px');
+					filter.setAttribute('style', 'width: ' + tgtWidth + 'px');
+				};
+			}
+		}, {
+			key: 'isBodyOverflowing',
+			value: function isBodyOverflowing() {
+				var body = this.element.querySelector('.grid-content-container');
+				return body.offsetHeight < body.scrollHeight || body.offsetWidth < body.scrollWidth;
 			}
 		}, {
 			key: 'gridHeight',
