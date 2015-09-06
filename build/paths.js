e6ea371222e61156586218f83c069372cd37be66
Charles Pockert <charles.pockert@googlemail.com>
2015-09-06 22:35:47 +0100

    Update paths.js

--- build/paths.js
+++ build/paths.js
@@ -13,7 +13,5 @@ module.exports = {
   doc:'./doc',
   e2eSpecsSrc: 'test/e2e/src/*.js',
   e2eSpecsDist: 'test/e2e/dist/',
-  //demo: 'C:/JMC/Development/TFSRoot/JMC/Internal/RSM/EmailMonitor/RSMConsole/RSMConsole/jspm_packages/github/charlespockert/aurelia-bs-grid@master'
-  //demo: '../aurelia-grid/jspm_packages/github/charlespockert/aurelia-bs-grid@0.0.2/',
-  demo: '../aurelia-bs-grid-demo/jspm_packages/github/charlespockert/aurelia-bs-grid@master/',
+  demo: '../aurelia-bs-grid-demo/jspm_packages/github/CharlesPockert/aurelia-bs-grid@master/',
 };
